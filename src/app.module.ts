92231144377ed1f8ffafd06d2b61b0ada79f311b
Ivan Herlan Herbas Zubieta <79385821+ivancidev@users.noreply.github.com>
2025-03-04 20:30:38 -0400

    feat(app): implement file upload functinality with Multer and serve static files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MulterModule } from '@nestjs/platform-express';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 @Module({
-  imports: [],
+  imports: [
+    MulterModule.register({
+      dest: './uploads',
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/file',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
